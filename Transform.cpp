2cc9d5cccf9cd6ec7297beed51800d9bf65634fc
pg22nicholas <pg22nicholas@vfs.com>
2022-06-11 13:30:05 -0700

    Added Parenting logic inside Transform
    Added constructor overloads for GameObject and Transform for setting parent

--- Transform.cpp
+++ Transform.cpp
@@ -1,13 +1,19 @@
 #include "Transform.h"
+#include "GameObject.h"
 
-
-
-Transform::Transform(GameObject* OwningGameObject) :Component(OwningGameObject)
+Transform::Transform(GameObject* OwningGameObject) :Component(OwningGameObject), mPosition(exVector3(1, 1, 1)), mParentGameObject(nullptr)
 { 
-    mPosition = exVector3(1, 1, 1);
 }
 
-Transform::Transform(GameObject* OwningGameObject, exVector3 position) : Component(OwningGameObject), mPosition(position)
+Transform::Transform(GameObject* OwningGameObject, exVector3 position) : Component(OwningGameObject), mPosition(position), mParentGameObject(nullptr)
+{
+}
+
+Transform::Transform(GameObject* OwningGameObject, GameObject* parentGameObject) : Component(OwningGameObject), mParentGameObject(parentGameObject)
+{
+}
+
+Transform::Transform(GameObject* OwningGameObject, exVector3 position, GameObject* parentGameObject) : Component(OwningGameObject), mPosition(exVector3(1, 1, 1)), mParentGameObject(parentGameObject)
 {
 }
 
@@ -19,4 +25,18 @@ ComponentTypes Transform::GetType()
 void Transform::SetPosition(exVector3 position)
 {
     mPosition = position;
+}
+
+GameObject* Transform::GetParent()
+{
+	return mParentGameObject;
+}
+
+exVector3 Transform::GetPosition() { return mPosition; }
+exVector3 Transform::GetLocalPosition() {
+
+	// TODO: Use handlers for gameObject references
+	if (mParentGameObject == nullptr) return mPosition;
+	exVector3 localVec = mParentGameObject->GetTransform()->GetLocalPosition() + mPosition;
+	return localVec;
 }
--- Transform.h
+++ Transform.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Component.h"
-
 #include "Game/Private/exVector3.h"
 
 class Transform :public Component
@@ -9,15 +8,21 @@ class Transform :public Component
 public:
 	Transform(GameObject* OwningGameObject);
 	Transform(GameObject* OwningGameObject, exVector3 position);
+	
+	Transform(GameObject* OwningGameObject, GameObject* parentGameObject);
+	Transform(GameObject* OwningGameObject, exVector3 position, GameObject* parentGameObject);
 
 	virtual ComponentTypes GetType() override;
 
-	exVector3 GetPosition(){ return mPosition; }
+	exVector3 GetPosition();
+	exVector3 GetLocalPosition();
 
 	void SetPosition(exVector3 position);
+	GameObject* GetParent();
 
 private:
 
 	exVector3 mPosition;
+	GameObject* mParentGameObject;
 };
 

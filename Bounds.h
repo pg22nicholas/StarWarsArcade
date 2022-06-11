40403ee9f59b319a1e6249efad0be24274ab3a7e
pg22nicholas <pg22nicholas@vfs.com>
2022-06-11 12:36:01 -0700

    added center point and origin in bounds

--- Bounds.h
+++ Bounds.h
@@ -1,14 +1,22 @@
 #pragma once
+#include "Engine/Public/EngineInterface.h"
+#include "Game/Private/exVector3.h"
 
 // Holds position bounds of GameObjects and simple related logic
 class Bounds {
 
 public:
+	static constexpr float centerX = (float) kViewportWidth / 2;
+	static constexpr float centerY = (float) kViewportHeight / 2;
 	static const int zBounds = 100;	// The distance on object can travel in z-direction before dissappearing
 	static const int xBounds = 100;	// distance the player/camera can travel left/right
 
 	// get the percent size of the gameObject based on its z-position, with a min of 0, max of zBounds
 	static float zSizePercentage(float z) {
 		return 1 - (z / zBounds);
+	}
+
+	static exVector3 GetOrigin() {
+		return exVector3(centerX, centerY, 0);
 	}
 };

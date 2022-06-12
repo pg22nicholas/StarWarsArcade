caf3955e1c6a7dcf610a2210886ac11d3eb5593f
Shatru <pg22shatrujit@vfs.com>
2022-06-12 09:47:01 -0700

    Reticle Controller inherits and implements it's methods

---
+++ ReticleController.cpp
@@ -0,0 +1,39 @@
+#include "ReticleController.h"
+
+ReticleController::ReticleController(GameObject* Owner) : ControllerComponent(Owner), mReticleInput(0), mFire(false) {}
+
+
+void ReticleController::ReadInput(const uint8_t* pState)
+{
+	mReticleInput |= pState[SDL_SCANCODE_UP] << UP_INPUT;
+	mReticleInput |= pState[SDL_SCANCODE_DOWN] << DOWN_INPUT;
+	mReticleInput |= pState[SDL_SCANCODE_LEFT] << LEFT_INPUT;
+	mReticleInput |= pState[SDL_SCANCODE_RIGHT] << RIGHT_INPUT;
+
+	SetReticleMovement();
+	//mFire |= pState[SDL_SCANCODE_SPACE];
+}
+
+void ReticleController::SetReticleMovement()
+{
+	PhysicsComponent* physicsComp = mOwningGameObject->FindComponent<PhysicsComponent>(ComponentTypes::Physics);
+	Transform* transform = mOwningGameObject->GetTransform();
+	exVector3 velocity = exVector3::Zero();
+
+	if ((mReticleInput & 1 << UP_INPUT) != 0) {
+		velocity += transform->GetUp();
+	}
+	else if ((mReticleInput & 1 << DOWN_INPUT) != 0) {
+		velocity += transform->GetUp() * -1;
+	}
+
+	if ((mReticleInput & 1 << RIGHT_INPUT) != 0) {
+		velocity += transform->GetRight();
+	}
+	else if ((mReticleInput & 1 << LEFT_INPUT) != 0) {
+		velocity += transform->GetRight() * -1;
+	}
+
+	physicsComp->SetVelocity(velocity.Normalize() * mSpeed);
+	mReticleInput = 0;
+}
---
+++ ReticleController.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include "ControllerComponent.h"
+#include "PhysicsComponent.h"
+#include "GameObject.h"
+#include "SDL2-2.0.3/include/SDL_scancode.h"
+#define LEFT_INPUT 0
+#define RIGHT_INPUT 1
+#define UP_INPUT 2
+#define DOWN_INPUT 3
+
+class ReticleController :
+    public ControllerComponent
+{
+public:
+	ReticleController(GameObject* Owner);
+
+	virtual void ReadInput(const uint8_t* pState) override;
+	virtual void SetReticleMovement() override;
+
+private:
+	int mReticleInput;
+	float mSpeed = 400;
+	bool mFire;
+};
+

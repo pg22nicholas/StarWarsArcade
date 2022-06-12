eaf87e15e294da595a954209e8449cff1e551f73
Shatru <pg22shatrujit@vfs.com>
2022-06-12 16:16:37 -0700

    Attack component stores reference to turrets to fire from

--- AttackComponent.h
+++ AttackComponent.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "Component.h"
+#include "Projectile.h"
 #include <Game/Private/exVector3.h>
 class AttackComponent :
     public Component
 {
 public:
-	AttackComponent(GameObject* Owner, float damage, int team);
+	AttackComponent(GameObject* Owner, float damage, int team, std::vector<GameObject*> turrets);
 	virtual ComponentTypes GetType() override;
 
 	void Fire(exVector3 heading);
@@ -13,5 +14,6 @@ public:
 private:
 	int mTeam;
 	float mDamage;
+	std::vector<GameObject*> mTurrets;
 };
 

a2c15b71b92f7a85a3b59344d16e2e69e3511d49
Shatru <pg22shatrujit@vfs.com>
2022-06-12 16:16:11 -0700

    Attack component stores reference to turrets to fire from

--- AttackComponent.cpp
+++ AttackComponent.cpp
@@ -1,13 +1,17 @@
 #include "AttackComponent.h"
 
-AttackComponent::AttackComponent(GameObject* Owner, float damage, int team):Component(Owner), mDamage(damage), mTeam(team) {}
+AttackComponent::AttackComponent(GameObject* Owner, float damage, int team, std::vector<GameObject*> turrets):Component(Owner), mDamage(damage), mTeam(team), mTurrets(turrets) {}
 
 ComponentTypes AttackComponent::GetType()
 {
 	return ComponentTypes::Attack;
 }
 
-void AttackComponent::Fire(exVector3 heading)
+void AttackComponent::Fire(exVector3 target)
 {
-	heading.Normalize();
+	for(GameObject* turret: mTurrets) {
+		exVector3 turretPosition = turret->GetTransform()->GetLocalPosition();
+		exVector3 direction = (target - turretPosition).Normalize();
+		new Projectile(turretPosition, direction, ColorTypes::RED);
+	}
 }

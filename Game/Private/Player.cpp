b1569e336bc1c0d7b7f023c49ff47ee5a1e06dff
Shatru <pg22shatrujit@vfs.com>
2022-06-12 16:20:09 -0700

    Send turret positions to Attack comp

--- Game/Private/Player.cpp
+++ Game/Private/Player.cpp
@@ -15,7 +15,11 @@ void Player::Initialize()
 	// Player has no rendering capabilities
 	AddComponent(new PhysicsComponent(this, false, 0, 0, exVector3()));
 	AddComponent(new PlayerController(this));
-	AddComponent(new AttackComponent(this, 1, 0));
+
+	std::vector<GameObject*> turrets;
+	turrets.push_back(new GameObject(exVector3(-Bounds::centerX, Bounds::centerY, 0), this));
+	turrets.push_back(new GameObject(exVector3(Bounds::centerX, Bounds::centerY, 0), this));
+	AddComponent(new AttackComponent(this, 1, 0, turrets));
 	AddComponent(new HealthComponent(this, 10, 0));
 	// TODO: health, and stat components
 

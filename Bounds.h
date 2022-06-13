02ac21fe23211659dcd619f8d7f90a38be618719
pg22nicholas <pg22nicholas@vfs.com>
2022-06-13 00:52:33 -0700

    Added Y bounds and updated X and Y bound values

--- Bounds.h
+++ Bounds.h
@@ -9,7 +9,8 @@ public:
 	static constexpr float centerX = (float) kViewportWidth / 2;
 	static constexpr float centerY = (float) kViewportHeight / 2;
 	static const int zBounds = 100;	// The distance on object can travel in z-direction before dissappearing
-	static const int xBounds = 100;	// distance the player/camera can travel left/right
+	static const int xBounds = 400;	// distance the player/camera can travel left/right
+	static const int YBounds = 200;	// distance the player/camera can travel up/down
 
 	// get the percent size of the gameObject based on its z-position, with a min of 0, max of zBounds
 	static float zSizePercentage(float z) {

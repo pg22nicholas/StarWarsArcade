5bd158dcd813555481b7c237b02163963cc2f434
Shatru <pg22shatrujit@vfs.com>
2022-06-12 18:08:54 -0700

    Pass team and damage to projectile

--- Projectile.cpp
+++ Projectile.cpp
@@ -1,16 +1,31 @@
 #include "Projectile.h"
 
-Projectile::Projectile(exVector3 origin, exVector3 heading, ColorTypes color):GameObject(origin),mHeading(heading),mColor(color) {
+Projectile::Projectile(exVector3 origin, exVector3 heading, ColorTypes color, int team, float damage):GameObject(origin),mHeading(heading),mColor(color),mTeam(team),mDamage(damage) {
 
 	Initialize();
 }
 
 void Projectile::Initialize()
 {
-	AddComponent(new PhysicsComponent(this, false, 0, 0, mHeading * mSpeed));
+	PhysicsComponent* physicsComponent = new PhysicsComponent(this, false, 0, 0, mHeading * mSpeed);
+	physicsComponent->AddColissionEventLitsner(this);
+	AddComponent(physicsComponent);
+
 	CircleComponent* circleComp = new CircleComponent(this, mRadius);
 	circleComp->SetColor(mColor);
 	AddComponent(circleComp);
 
 	GameObject::Initialize();
+}
+
+void Projectile::OnCollision(PhysicsComponent* pCurrentComponent, PhysicsComponent* pOtherComponent)
+{
+	if (FindComponent<PhysicsComponent>(ComponentTypes::Physics) != pCurrentComponent) return;
+	HealthComponent* otherHealth = pOtherComponent->GetGameObject()->FindComponent<HealthComponent>(ComponentTypes::Health);
+
+	if (otherHealth == nullptr) return;
+	if (!otherHealth->CanHit(mTeam)) return;
+
+	otherHealth->TryDamage(mDamage, mTeam);
+	Expire();
 }

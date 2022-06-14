76f03ddb19e66dce21ee0296edb77dd1beb1bcbd
pg22shatrujit@vfs.com <pg22shatrujit@vfs.com>
2022-06-14 16:20:03 -0700

    Setup object handle and manager

---
+++ GameObjectManager.cpp
@@ -0,0 +1,44 @@
+#include "GameObjectManager.h"
+
+// Singleton manager to track hashmap of existing gameobjects 
+GameObjectManager* GameObjectManager::sInstance = nullptr;
+GameObjectManager* GameObjectManager::GetInstance()
+{
+    if (sInstance == nullptr) sInstance = new GameObjectManager();
+    return sInstance;
+}
+
+// Register GameObjects at construction
+void GameObjectManager::RegisterGameObject(GameObject* objectToRegister)
+{
+    int index = objectToRegister->GetID();
+    mGameObjectMap.emplace(index, objectToRegister);
+}
+
+// Deregister gameobjects  and destruction
+void GameObjectManager::DeregisterGameObject(GameObject* objectToDeregister)
+{
+    mGameObjectMap.erase(objectToDeregister->GetID());
+}
+
+// Find object from Hash
+GameObject* GameObjectManager::Lookup(int hash)
+{
+    auto iterator = mGameObjectMap.find(hash);
+    if (iterator != mGameObjectMap.end()) return iterator->second;
+    return nullptr;
+}
+
+// Check if an object exists in the map
+bool GameObjectManager::Exist(GameObject* objectToCheck)
+{
+    if (objectToCheck == nullptr) return false;
+    return Lookup(objectToCheck->GetID()) == objectToCheck;
+}
+
+int GameObjectManager::GenerateID()
+{
+    return mGameObjectMap.size();
+}
+
+GameObjectManager::GameObjectManager() {}
---
+++ GameObjectManager.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <map>
+#include "GameObjectHandle.h"
+
+class GameObject;
+class GameObjectManager
+{
+public:
+	static GameObjectManager* GetInstance();
+	void RegisterGameObject(GameObject* objectToRegister);
+	void DeregisterGameObject(GameObject* objectToDeregister);
+
+	GameObject* Lookup(int hash);
+	bool Exist(GameObject* objectToCheck);
+	int GenerateID();
+
+private:
+	std::map<int, GameObject*> mGameObjectMap;
+	GameObjectManager();
+	static GameObjectManager* sInstance;
+};
+
